a499cbdca8b36ee1c65dea7dba0e441030a6f32d
Dipec001 <dpecchukwu@gmail.com>
2023-10-03 09:15:41 +0100

    endpoint conf

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     # Define the URL pattern for video upload
     path('api/upload_video/', views.upload_video, name='upload_video'),
-    path('video/play/<int:video_id>/', views.video_playback, name='video_playback'),
+    path('api/video/play/<int:video_id>/', views.video_playback, name='video_playback'),
     path('api/videos/', views.ListVideosView.as_view(), name='list_videos'),
     path('api/get_transcription/<int:video_id>/', views.get_transcription, name='get_transcription'),
     # path('start_transcription/', views.start_transcription, name='start_transcription'),
